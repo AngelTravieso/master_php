28d9e2d8f6b9e6658d2a2f267110628e884723d3
Angel Travieso <68413529+AngelTravieso@users.noreply.github.com>
2021-10-13 23:36:18 -0400

    Commit inicial

---
+++ PHP_VR/03-variables.php
@@ -0,0 +1,17 @@
+<?php
+
+	$variable = 'Hola Mundo desde una variable';
+	$numero = 22;
+	$verdadero = true;
+
+	$numero = 54;
+
+
+	echo '<h1>' . $variable . '</h1>';
+	echo $numero . '<br>';
+
+	$numero = 120;
+
+	echo $numero;
+
+?>
